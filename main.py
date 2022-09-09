ff982cbedc597c96e999e034064a38886faf8aef
Lukasz S <ukasz668@gmail.com>
2022-09-09 11:47:23 +0200

    commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 
 screen = turtle.Screen()
@@ -7,6 +8,29 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 
 turtle.shape(image)
+
+states = pandas.read_csv("50_states.csv")
+all_states = states.state.to_list()
+
+
+
+quest = screen.textinput(title="Quest the state", prompt="What another states name?")
+correct = set()
+while True:
+
+    if quest in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = states[states.state == quest]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(quest)
+        correct.add(quest)
+
+    counter = len(correct)
+    quest = screen.textinput(title=f"{counter}/50 Quest the state", prompt="What another states name?")
+
+    #turtle.mainloop()
 
 
 
